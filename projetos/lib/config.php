9ba22a95598656ff9c04e5e12b1e84424168136c
bruno.godoi <bruno.godoi@4linux.com.br>
2020-01-29 22:25:17 -0300

    inicio do projeto

---
+++ projetos/lib/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = [
+    'database' => [
+        'host' => 'localhost',
+        'user' => 'root',
+        'pass' => 'h4ck3r',
+        'db' => 'projetos'
+    ]
+];
---
+++ projetos/paginas/login.php
@@ -0,0 +1,19 @@
+<?php
+    
+?>
+<div class="container">
+<form method="post">
+  <div class="form-group">
+    <label for="username">Usuário</label>
+    <input type="text" class="form-control" id="username" name="username">
+  </div>
+  <div class="form-group">
+    <label for="senha">Senha</label>
+    <input type="password" class="form-control" id="senha" name="senha">
+  </div>
+  <button type="submit" class="btn btn-success">Entrar</button>
+</form>
+
+<br>
+<a class="btn btn-large btn-primary" href="?pagina=registro">Registrar-se</a>
+</div>
